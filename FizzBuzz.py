b44e68afc8de220e932723fc2e7e20f208b101ba
Paku-Paku <masa_o6o9_chil@yahoo.co.jp>
2018-06-13 10:50:49 +0900

    add description

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,12 @@
+"""
+3で割り切れる場合はFizz
+5で割り切れる場合はBuzz
+３でも5でも割り切れる場合はFizzBuzz
+"""
+
 def fizzbuzz(number=100):
     for i in range(1, number+1):
-        if i % 15 == 0:
+        if i % 15 == 0:        
             print("FizzBuzz")
         elif i % 3 == 0:
             print("Fizz")
